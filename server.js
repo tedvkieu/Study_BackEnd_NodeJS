0562f2216e9f4f2ca6f70cb4a5970b09509c1338
kieuishere <dvkieu165@gmail.com>
2023-11-10 21:29:55 +0700

    Part 17

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/abc', (req, res) => {
+    res.send('Check ABC')
+})
+
+app.get('/tedvkieu', (req, res) => {
+    res.send('<h1>TEDVKIEU</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
