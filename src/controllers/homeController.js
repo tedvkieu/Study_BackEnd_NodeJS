fc0ae825cafdfa1502dda68f724f85029e49c649
kieuishere <dvkieu165@gmail.com>
2023-11-12 22:05:53 +0700

    Part 32: Connection Cost

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,19 @@
-const getHomePage = (req, res)=>{
-    res.send('Hello World with Kieu')
+const connection = require('../config/database.js');
+
+const getHomePage = (req, res) => {
+    let users = [];
+    connection.query(
+        'SELECT * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>> result: ", results); // results contains rows returned by server
+            //res.send(JSON.stringify(users));
+            console.log("check user: ", users);
+            //res.send('Hello World with Kieu')
+            res.send(JSON.stringify(users));
+        }
+    );
+    
 }
 
 const getABC = (req, res)=>{
--- src/server.js
+++ src/server.js
@@ -11,14 +11,14 @@ configViewEngine(app);
 app.use('/', webRoute)
 
 
-// simple query
+/* simple query
 connection.query(
   'SELECT * FROM Users u',
   function(err, results, fields) {
     console.log(">>> result: ", results); // results contains rows returned by server
     //console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
   }
-);
+);*/
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
