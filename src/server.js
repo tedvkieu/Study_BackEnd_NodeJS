f11808aff417fe62f56e71f4a2e05fed5cd7c86b
kieuishere <dvkieu165@gmail.com>
2023-11-12 14:56:18 +0700

    Part 30: Use DataBase with Nodejs

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
-const host = process.env.HOST_NAME
+const hostname = process.env.HOST_NAME
+const mysql = require('mysql2')
 const webRoute = require('./routes/web.js')
 const configViewEngine = require('./config/viewEngine.js');
 
 configViewEngine(app);
 app.use('/', webRoute)
 
-app.listen(port, () => {
+//test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'tedvkieu'
+});
+// simple query
+connection.query(
+  'SELECT * FROM Users u',
+  function(err, results, fields) {
+    console.log(">>> result: ", results); // results contains rows returned by server
+    console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
+  }
+);
+
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
