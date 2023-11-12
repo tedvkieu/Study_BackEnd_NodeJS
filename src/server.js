759bee67ba24803b8022f3196f1e53354be10dba
kieuishere <dvkieu165@gmail.com>
2023-11-12 21:38:25 +0700

    Part 31: Use Connection

--- src/server.js
+++ src/server.js
@@ -3,27 +3,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME
-const mysql = require('mysql2')
+const connection = require('./config/database.js')
 const webRoute = require('./routes/web.js')
 const configViewEngine = require('./config/viewEngine.js');
 
 configViewEngine(app);
 app.use('/', webRoute)
 
-//test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '123456',
-  database: 'tedvkieu'
-});
+
 // simple query
 connection.query(
   'SELECT * FROM Users u',
   function(err, results, fields) {
     console.log(">>> result: ", results); // results contains rows returned by server
-    console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
+    //console.log(">>> fields: ", fields); // fields contains extra meta data about results, if available
   }
 );
 
