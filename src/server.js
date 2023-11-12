475b08a0aa46af17a7e7ec519a9305ad63886f46
kieuishere <dvkieu165@gmail.com>
2023-11-12 09:02:03 +0700

    Part 21: Static Files

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const host = process.env.HOST_NAME
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public'))) //Select the file path as "public" to find subfiles 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
