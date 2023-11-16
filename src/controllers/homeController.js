a69c58f3623aa2724e99a6fe2aabb16e19d7a6dd
kieuishere <dvkieu165@gmail.com>
2023-11-16 11:55:57 +0700

    38. Chức Năng Create User

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,25 +1,38 @@
 const connection = require('../config/database.js');
 
 const getHomePage = (req, res) => {
-    
     return res.render('homePage.ejs');
-    
-}
+};
 
-const getABC = (req, res)=>{
-    res.send('Hello ABC')
-}
-const getTed = (req, res)=>{
-    res.render('sample.ejs')
-}
+const getABC = (req, res) => {
+    res.send('Hello ABC');
+};
+const getTed = (req, res) => {
+    res.render('sample.ejs');
+};
 
 const postCreateUser = (req, res) => {
-    console.log(">>> res.body: ", req.body)
-    res.send("create new user");
-}
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+
+    console.log('>>> email = : ', email, 'name= ', name, 'city = ', city);
+
+    connection.query(
+        `INSERT INTO Users (email, name, city) 
+        VALUES(?,?,?)`,
+        [email, name, city],
+
+        function (err, results) {
+            console.log(results);
+            res.send('create User success!!');
+        }
+    );
+};
 
 module.exports = {
     getHomePage,
     getABC,
-    getTed, postCreateUser
-}
+    getTed,
+    postCreateUser,
+};
