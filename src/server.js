427c65d6a46a9a5e9121449c1c27b0e553e1e0c3
kieuishere <dvkieu165@gmail.com>
2023-11-25 14:12:15 +0700

    Part 59. Create Database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
+const mongoose = require('mongoose');
 const connection = require('./config/database.js');
 const webRoute = require('./routes/web.js');
 const configViewEngine = require('./config/viewEngine.js');
@@ -13,6 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 
 configViewEngine(app);
 app.use('/', webRoute);
+
+const kittySchema = new mongoose.Schema({
+    name: String,
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'Kieu ne' });
+
+cat.save();
 
 (async () => {
     try {
