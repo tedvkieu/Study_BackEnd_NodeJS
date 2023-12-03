6a85a052e4b9ae9ef07c006c9a076939b40ffb1b
kieuishere <dvkieu165@gmail.com>
2023-12-03 09:41:32 +0700

    Part 81. Model Customers

---
+++ src/models/customer.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// create shape
+const customerSchema = new mongoose.Schema(
+    {
+        name: { type: String, require: true },
+        address: String,
+        phone: String,
+        email: String,
+        image: String,
+        description: String,
+    },
+    { timestamps: true }
+);
+
+//save down database
+const Customer = mongoose.model('user', customerSchema);
+
+module.exports = Customer;
